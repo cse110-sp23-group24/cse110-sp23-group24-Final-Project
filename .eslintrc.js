f44ceb8125681297e4ae94696536fb52117839db
sumitshetye2 <sshetye@ucsd.edu>
2023-05-16 17:50:45 -0700

    elsint test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-  },
-  'extends': 'google',
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
-  },
-  "rules": {
-    "indent": ["error", 4],
-    "quotes": ["error", "double"],
-    "semi": ["error", "always"],
-    "no-unused-vars": "warn"
-  }
+    'env': {
+      'browser': true,
+      'es2021': true,
+    },
+    'extends': 'google',
+    'overrides': [
+    ],
+    'parserOptions': {
+      'ecmaVersion': 'latest',
+      'sourceType': 'module',
+    },
+    "rules": {
+      "indent": ["error", 4],
+      "quotes": ["error", "double"],
+      "semi": ["error", "always"],
+      "no-unused-vars": "warn"
+    }
 };
+
+
