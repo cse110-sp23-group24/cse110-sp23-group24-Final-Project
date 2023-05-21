d8e0a35796aabf94f7b50a7a41e2775591331338
Nho-way <93555421+Nho-way@users.noreply.github.com>
2023-05-21 14:51:44 -0700

    code climate

---
+++ scripts/codeClimateTest.js
@@ -0,0 +1,12 @@
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Done");
+
