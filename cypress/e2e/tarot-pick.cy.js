d35e5c2e38629d1fabca40da0ab7a56d78491e7c
Sumit Shetye <sshetye@ucsd.edu>
2023-06-08 18:38:00 -0700

    updated e2e files

--- cypress/e2e/tarot-pick.cy.js
+++ cypress/e2e/tarot-pick.cy.js
@@ -6,25 +6,25 @@ describe('Tarot card pick page tests', () => {
     it('should display the correct title', () => {
         cy.visit('http://localhost:5173/pages/select-cards/');
         cy.get('.select-cards-title').should(
-          'contain',
-          'Please choose 3 cards for your reading'
+            'contain',
+            'Please choose 3 cards for your reading'
         );
     });
 
     it('should have a cards container', () => {
         cy.visit('http://localhost:5173/pages/select-cards/');
-        // Wrap the assertion inside a cy.then() to handle the asynchronous behavior
+        // Wrap the assertion inside a cy.then() to handle the async behavior
         cy.get('.cards-container').then(($cardsContainer) => {
-          expect($cardsContainer).to.exist;
+            expect($cardsContainer).to.exist;
         });
     });
 
     it('should load the cards', () => {
         cy.visit('http://localhost:5173/pages/select-cards/');
-        // Wrap the action inside a cy.then() to handle the asynchronous behavior
+        // Wrap the action inside a cy.then() to handle the async behavior
         cy.get('.cards-container').then(($cardsContainer) => {
           // Wait for the cards to load by checking if the expected number of cards are present
-          cy.get('.tarot-card').should('have.length', 78);
+            cy.get('.tarot-card').should('have.length', 78);
         });
     });
 
@@ -34,9 +34,8 @@ describe('Tarot card pick page tests', () => {
       
         // Assert that the length of selected cards in local storage is less than 3
         cy.window().then((win) => {
-          const storedCards = JSON.parse(win.localStorage.getItem('FutureNowState')).TarotState.selectedCards;
-          expect(storedCards.length).to.be.lessThan(3);
+            const storedCards = JSON.parse(win.localStorage.getItem('FutureNowState')).TarotState.selectedCards;
+            expect(storedCards.length).to.be.lessThan(3);
         });
     });
-      
-}); 
+});
