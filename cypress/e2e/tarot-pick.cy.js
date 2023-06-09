bf08179065e2c4109390742fb2631983f36660b1
beliang <b3nliang@gmail.com>
2023-06-08 20:23:12 -0700

    Added select 3 cards to result page test

--- cypress/e2e/tarot-pick.cy.js
+++ cypress/e2e/tarot-pick.cy.js
@@ -1,10 +1,11 @@
 describe('Tarot card pick page tests', () => {
     beforeEach(() => {
         cy.visit('http://localhost:5173/');
+        cy.contains('Get Started').click();
     });
 
     it('should display the correct title', () => {
-        cy.visit('http://localhost:5173/pages/select-cards/');
+        //cy.visit('http://localhost:5173/pages/select-cards/');
         cy.get('.select-cards-title').should(
             'contain',
             'Please choose 3 cards for your reading'
@@ -12,7 +13,7 @@ describe('Tarot card pick page tests', () => {
     });
 
     it('should have a cards container', () => {
-        cy.visit('http://localhost:5173/pages/select-cards/');
+        //cy.visit('http://localhost:5173/pages/select-cards/');
         // Wrap the assertion inside a cy.then() to handle the async behavior
         cy.get('.cards-container').then(($cardsContainer) => {
             expect($cardsContainer).to.exist;
@@ -20,7 +21,7 @@ describe('Tarot card pick page tests', () => {
     });
 
     it('should load the cards', () => {
-        cy.visit('http://localhost:5173/pages/select-cards/');
+        //cy.visit('http://localhost:5173/pages/select-cards/');
         // Wrap the action inside a cy.then() to handle the async behavior
         cy.get('.cards-container').then(($cardsContainer) => {
           // Wait for the cards to load by checking if the expected number of cards are present
@@ -30,7 +31,7 @@ describe('Tarot card pick page tests', () => {
 
     // WIP wait for integration of results page 
     it('should not allow selecting more than 3 cards', () => {
-        cy.visit('http://localhost:5173/pages/select-cards/');
+        //cy.visit('http://localhost:5173/pages/select-cards/');
       
         // Assert that the length of selected cards in local storage is less than 3
         cy.window().then((win) => {
@@ -38,4 +39,35 @@ describe('Tarot card pick page tests', () => {
             expect(storedCards.length).to.be.lessThan(3);
         });
     });
+
+    it('should redirect to the next page after selecting three cards', () => {
+        // Visit the select cards page
+        //cy.visit('/select-cards-page');
+        
+        // Simulate card selection (replace the selectors and actions with your own)
+        cy.get('tarot-card').eq(0).click(); // Select card 1
+        cy.wait(3000);
+        cy.get('body').click();
+        cy.get('tarot-card').eq(1).click(); // Select card 2
+        cy.wait(3000);
+        cy.get('body').click();
+        cy.get('tarot-card').eq(2).click(); // Select card 3
+        cy.wait(3000);
+        cy.get('body').click();
+        
+        // Verify the redirection to the next page
+        cy.url().should('include', '/pages/result-page');
+        
+        cy.get('h1').should('contain', 'Tarot Card Reading');
+        cy.get('p.label').each(($p, index) => {
+            // Perform assertions based on the index
+            if (index === 0) {
+              cy.wrap($p).should('contain', 'Past');
+            } else if (index === 1) {
+              cy.wrap($p).should('contain', 'Present');
+            } else if (index === 2) {
+              cy.wrap($p).should('contain', 'Future');
+            }
+        });
+      });
 });
