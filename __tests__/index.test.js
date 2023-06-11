e89988084a73441d66f0af7ca6a7f994c02741d2
beliang2 <bzliang@ucsd.edu>
2023-06-11 02:10:26 -0700

    shuffleArray test added

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,10 +4,10 @@ const func = require('../pages/select-cards/index.js');
 describe('shuffle array', () => {
     test('should shuffle the array', () => {
         const originalArray = [1, 2, 3, 4, 5];
-        //const shuffledArray = func.shuffleArray(originalArray);
-
+        const shuffledArray = func.shuffleArray([...originalArray]);
+        
         // Verify that the shuffled array is not equal to the original array
-        expect(func.shuffleArray(originalArray)).not.toEqual(originalArray);
+        expect(shuffledArray).not.toEqual(originalArray);
 
         // Verify that the shuffled array contains all the same elements as the original array
         expect(shuffledArray).toEqual(expect.arrayContaining(originalArray));
