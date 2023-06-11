51d7870792eb4415da06d44a33188fb96447cf15
Sumit Shetye <sshetye@ucsd.edu>
2023-06-11 02:05:10 -0700

    updated files for unit testing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,13 @@
 // import { shuffleArray } from '../pages/select-cards/index.js';
-const functions = require('../pages/select-cards/index.js');
+const func = require('../pages/select-cards/index.js');
 
 describe('shuffle array', () => {
     test('should shuffle the array', () => {
         const originalArray = [1, 2, 3, 4, 5];
-        const shuffledArray = functions.shuffleArray(originalArray);
+        //const shuffledArray = func.shuffleArray(originalArray);
 
         // Verify that the shuffled array is not equal to the original array
-        expect(shuffledArray).not.toEqual(originalArray);
+        expect(func.shuffleArray(originalArray)).not.toEqual(originalArray);
 
         // Verify that the shuffled array contains all the same elements as the original array
         expect(shuffledArray).toEqual(expect.arrayContaining(originalArray));
--- __tests__/setupTests.js
+++ __tests__/setupTests.js
@@ -2,11 +2,29 @@ class HTMLElement {}
 global.HTMLElement = HTMLElement;
 global.customElements = {
     define: jest.fn(),
-  };
-  global.performance = {
-    getEntriesByType: jest.fn().mockReturnValue([{ type: 'reload' }]),
-  };
+};
+global.performance = {
+  getEntriesByType: jest.fn().mockReturnValue([{ type: 'reload' }]),
+};
+global.localStorage = {
+  getItem: jest.fn().mockReturnValue('mockedValue'),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+};
+global.window = {
+    addEventListener: jest.fn(),
+};
+global.window.location = {
+    href: '',
+};
+global.Audio = jest.fn().mockImplementation(() => ({
+    play: jest.fn(),
+    pause: jest.fn(),
+    loop: false,
+}));
   
+  
+
 
 describe('proxy', () => {
     test('should shuffle the array', () => {
